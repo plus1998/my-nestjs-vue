3269448b52b97f80ef152b4441ec45476a82edc1
happyplus <plus.1998@qq.com>
2026-03-24 17:29:17 +0800

    chore: reset db

---
+++ apps/server/src/database/migrations/1774344250691-InitSchema.ts
@@ -0,0 +1,18 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class InitSchema1774344250691 implements MigrationInterface {
+  name = 'InitSchema1774344250691';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE \`users\` (\`id\` varchar(36) NOT NULL, \`username\` varchar(191) NOT NULL, \`password_hash\` varchar(255) NOT NULL, \`created_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), \`updated_at\` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), UNIQUE INDEX \`IDX_fe0bb3f6520ee0469504521e71\` (\`username\`), PRIMARY KEY (\`id\`)) ENGINE=InnoDB`,
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `DROP INDEX \`IDX_fe0bb3f6520ee0469504521e71\` ON \`users\``,
+    );
+    await queryRunner.query(`DROP TABLE \`users\``);
+  }
+}
